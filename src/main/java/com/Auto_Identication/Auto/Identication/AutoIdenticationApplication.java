379ddf1d5e1b7af61ab13b6e31498c86a44e39f5
karuturisarathkumar <karuturisarathkumar@gmail.com>
2020-04-20 13:06:33 +0530

    committed by sarath

---
+++ src/main/java/com/Auto_Identication/Auto/Identication/AutoIdenticationApplication.java
@@ -0,0 +1,13 @@
+package com.Auto_Identication.Auto.Identication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoIdenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoIdenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Auto_Identication/Auto/Identication/AutoIdenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auto_Identication.Auto.Identication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoIdenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
